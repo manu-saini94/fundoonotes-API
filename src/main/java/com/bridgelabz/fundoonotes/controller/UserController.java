6e46d85c2f709b97c0af196043a0398551246c06
manu-saini94 <manu.saini94@gmail.com>
2020-01-13 00:51:26 +0530

    [manu]fundoonotes UserConfiguration updated

--- src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
+++ src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.fundoonotes.controller;
 
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.fundoonotes.dto.UserDTO;
 
 @RestController
 public class UserController {
@@ -9,5 +18,10 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@PostMapping("/users")
+	public Map userCreation(@Valid @RequestBody UserDTO userdto,BindingResult bindingresult)
+	{
+		
+	}
 	
 }
