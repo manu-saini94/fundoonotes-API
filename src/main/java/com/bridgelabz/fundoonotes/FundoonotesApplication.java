bad9e04af44cca5fa2a0901f1f203fb8d3197f7a
manu-saini94 <manu.saini94@gmail.com>
2020-01-12 23:15:42 +0530

    [manu]fundoonotes LoginDTO created

--- src/main/java/com/bridgelabz/fundoonotes/FundoonotesApplication.java
+++ src/main/java/com/bridgelabz/fundoonotes/FundoonotesApplication.java
@@ -2,8 +2,9 @@ package com.bridgelabz.fundoonotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class FundoonotesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bridgelabz/fundoonotes/repository/LoginRepository.java
+++ src/main/java/com/bridgelabz/fundoonotes/repository/LoginRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+
 import com.bridgelabz.fundoonotes.model.UserInfo;
 
 @Repository
@@ -12,4 +13,5 @@ public interface LoginRepository extends JpaRepository<UserInfo,String> {
 	@Query("from UserInfo where username=?1")
 	UserInfo findByUsername(String username);
 
+	
 }
