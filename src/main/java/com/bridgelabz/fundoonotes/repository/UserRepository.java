b77c7c86d6c14995c5c8862b9c1e467d8af26299
manu-saini94 <manu.saini94@gmail.com>
2020-01-13 18:38:39 +0530

    [manu]fundoonotes UserRepository.java updated

--- src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
+++ src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
@@ -1,5 +1,29 @@
 package com.bridgelabz.fundoonotes.repository;
 
-public interface UserRepository {
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.bridgelabz.fundoonotes.model.UserInfo;
+
+@Repository
+@Transactional
+public interface UserRepository extends JpaRepository<UserInfo,Integer> {
+
+	@Query(value="insert into user_info(id,username,firstname,lastname,email,password) values(:id,:username,:firstname,:lastname,:email,:password)",nativeQuery=true)
+	@Modifying	
+	public Integer SaveUser(int id,String username,String firstname,String lastname,String email,String password);
+
+	@Query("from UserInfo where username=?1")
+    UserInfo findByUsername(String username);
+	
+	@Query("from UserInfo where email=?1")
+	UserInfo findByEmail(String email);
+	
+	@Query(value="update user_info set isemailverified=true where username=:username",nativeQuery=true)
+	@Modifying
+	public void setVerifiedEmail(String username);
 }
