f2c500a097fa6ee90b009865a69276446552cc40
manu-saini94 <manu.saini94@gmail.com>
2020-01-16 15:56:17 +0530

    [manu]fundoonotes-API Exceptions updated

---
+++ src/main/java/com/bridgelabz/fundoonotes/Exceptions/JWTTokenException.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.fundoonotes.Exceptions;
+
+public class JWTTokenException extends Exception
+{
+	
+	private static final long serialVersionUID = 1L;
+
+	public JWTTokenException(String message)
+	 {
+		 super(message);
+	 }
+
+}
